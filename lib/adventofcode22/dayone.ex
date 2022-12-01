a06a8c5d80d0e15c263397228db6e683face1166
Aish <aish@aishraj.com>
2022-11-30 21:20:40 -0800

    Part 2

--- lib/adventofcode22/dayone.ex
+++ lib/adventofcode22/dayone.ex
@@ -1,13 +1,26 @@
 defmodule Adventofcode22.DayOne do
+  def read_input(input) do
+    input
+    |> File.read!()
+    |> String.split("\n\n")
+    |> Enum.map(&String.trim/1)
+    |> Enum.map(&String.split(&1, "\n"))
+  end
+
   def part_one(input) do
     input
-    |> Stream.map(&String.to_integer/1)
-    |> Enum.sum()
+    |> Enum.map(fn x -> Enum.map(x, &String.to_integer/1) end)
+    |> Enum.map(&Enum.sum(&1))
+    |> Enum.max()
   end
 
   def part_two(input) do
     input
-    |> Stream.map(&String.to_integer/1)
-    |> Enum.reduce(1, &(&1 * &2))
+    |> Enum.map(fn x -> Enum.map(x, &String.to_integer/1) end)
+    |> Enum.map(&Enum.sum(&1))
+    |> Enum.sort()
+    |> Enum.reverse()
+    |> Enum.take(3)
+    |> Enum.sum()
   end
 end
--- test/adventofcode22/dayone_test.exs
+++ test/adventofcode22/dayone_test.exs
@@ -1,31 +1,36 @@
 defmodule Adventofcode22.DayoneTest do
+  alias Mix.Tasks.Iex
   use ExUnit.Case
   doctest Adventofcode22
 
-  test "part one easy" do
-  end
+  # test "part one easy" do
+  #   data = ["1"]
+  #   result = Adventofcode22.DayOne.part_one(data)
+  #   assert result == 1
+  # end
 
-  test "part two easy" do
-  end
+  # test "part two easy" do
+  #   data = ["2"]
+  #   result = Adventofcode22.DayOne.part_two(data)
+  #   assert result == 2
+  # end
 
   test "part one" do
     ## Test runners should read the input from a file
     data =
-      Adventofcode22.read_input("input/1.txt")
+      Adventofcode22.DayOne.read_input("input/1.txt")
       |> Enum.to_list()
-      |> List.flatten()
 
     result = Adventofcode22.DayOne.part_one(data)
-    assert result == 8
+    assert result == 69626
   end
 
   test "part two" do
     data =
-      Adventofcode22.read_input("input/1.txt")
+      Adventofcode22.DayOne.read_input("input/1.txt")
       |> Enum.to_list()
-      |> List.flatten()
 
     result = Adventofcode22.DayOne.part_two(data)
-    assert result == 10
+    assert result == 206_780
   end
 end
