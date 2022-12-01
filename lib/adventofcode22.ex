79ca4de1dce4d009dadef5e5bb9a394ff27dbd75
Aish <aish@aishraj.com>
2022-11-30 17:06:18 -0800

    Bootstrap a new project

---
+++ lib/adventofcode22.ex
@@ -0,0 +1,18 @@
+defmodule Adventofcode22 do
+  @moduledoc """
+  Documentation for `Adventofcode22`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Adventofcode22.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/adventofcode22_test.exs
@@ -0,0 +1,8 @@
+defmodule Adventofcode22Test do
+  use ExUnit.Case
+  doctest Adventofcode22
+
+  test "greets the world" do
+    assert Adventofcode22.hello() == :world
+  end
+end
