011e7208fda66fba54a8f392439e59a75f3f077a
Aish <aish@aishraj.com>
2022-12-01 21:45:02 -0800

    Shrug

--- lib/adventofcode22/daytwo.ex
+++ lib/adventofcode22/daytwo.ex
@@ -1,7 +1,52 @@
 defmodule Adventofcode22.Daytwo do
+  def read_file(input) do
+    input
+    |> File.read!()
+    |> String.split("\n")
+    |> Enum.map(&String.trim/1)
+    |> Enum.map(&String.split(&1, " "))
+  end
+
   def part_one(input) do
     input
+    |> Enum.map(fn [x, y] -> calculate_score(x, y) end)
+    |> Enum.sum()
   end
+
+  defp calculate_score(x, y) do
+    a = item(x)
+    b = item(y)
+    result = compete(a, b)
+
+    base = score(a)
+
+    points = find_winner(result, a)
+
+    base + points
+  end
+
+  defp find_winner(result, p) when result == p, do: 6
+  defp find_winner(:draw, _), do: 3
+  defp find_winner(result, p) when result != p, do: 0
+
+  defp compete(:rock, :paper), do: :paper
+  defp compete(:paper, :rock), do: :paper
+  defp compete(:rock, :scissors), do: :rock
+  defp compete(:scissors, :rock), do: :rock
+  defp compete(:scissors, :paper), do: :scissors
+  defp compete(:paper, :scissors), do: :scissors
+  defp compete(x, x), do: :draw
+
+  defp item("A"), do: :rock
+  defp item("B"), do: :paper
+  defp item("C"), do: :scissors
+  defp item("X"), do: :rock
+  defp item("Y"), do: :paper
+  defp item("Z"), do: :scissors
+
+  defp score(:rock), do: 1
+  defp score(:paper), do: 2
+  defp score(:scissors), do: 3
 
   def part_two(input) do
     input
--- test/adventofcode22/daytwo_test.exs
+++ test/adventofcode22/daytwo_test.exs
@@ -1,13 +1,13 @@
-defmodule Adventofcode22.DayoneTest do
+defmodule Adventofcode22.DaytwoTest do
   alias Mix.Tasks.Iex
   use ExUnit.Case
   doctest Adventofcode22
 
-  # test "part one easy" do
-  #   data = ["1"]
-  #   result = Adventofcode22.DayOne.part_one(data)
-  #   assert result == 1
-  # end
+  test "part one easy" do
+    data = [["A", "Y"], ["B", "X"], ["C", "Z"]]
+    result = Adventofcode22.Daytwo.part_one(data)
+    assert result == 15
+  end
 
   # test "part two easy" do
   #   data = ["2"]
@@ -15,15 +15,15 @@ defmodule Adventofcode22.DayoneTest do
   #   assert result == 2
   # end
 
-  # test "part one" do
-  #   ## Test runners should read the input from a file
-  #   data =
-  #     Adventofcode22.DayOne.read_input("input/1.txt")
-  #     |> Enum.to_list()
+  test "part one" do
+    ## Test runners should read the input from a file
+    data =
+      Adventofcode22.Daytwo.read_file("input/2.txt")
+      |> Enum.to_list()
 
-  #   result = Adventofcode22.DayOne.part_one(data)
-  #   assert result == 69626
-  # end
+    result = Adventofcode22.Daytwo.part_one(data)
+    assert result == 69626
+  end
 
   # test "part two" do
   #   data =
