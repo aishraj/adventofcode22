b15944ee51bf089a62d1c64188ab94d8b4f8fe88
Aish <aish@aishraj.com>
2022-12-08 07:50:54 -0800

    day 8 part 1 done

--- lib/adventofcode22/dayeight.ex
+++ lib/adventofcode22/dayeight.ex
@@ -16,7 +16,77 @@ defmodule Adventofcode22.Dayeight do
     end)
   end
 
+  # TODO: DRY the following into a function that accepts a function in addition to the direction
+
+  defp is_visible(:left, grid, row, col) do
+    Enum.reduce(0..(col - 1), true, fn colnum, acc ->
+      if grid[row][colnum] >= grid[row][col] do
+        false
+      else
+        acc
+      end
+    end)
+  end
+
+  defp is_visible(_dir, _grid, 0, _col), do: true
+  defp is_visible(_dir, _grid, _row, 0), do: true
+
+  defp is_visible(:right, grid, row, col) do
+    boundary = Map.keys(grid[0]) |> Enum.max()
+
+    Enum.reduce((col + 1)..boundary, true, fn colnum, acc ->
+      if grid[row][colnum] >= grid[row][col] do
+        false
+      else
+        acc
+      end
+    end)
+  end
+
+  defp is_visible(:up, grid, row, col) do
+    Enum.reduce(0..(row - 1), true, fn rownum, acc ->
+      if grid[rownum][col] >= grid[row][col] do
+        false
+      else
+        acc
+      end
+    end)
+  end
+
+  defp is_visible(:down, grid, row, col) do
+    boundary = Map.keys(grid) |> Enum.max()
+
+    Enum.reduce((row + 1)..boundary, true, fn rownum, acc ->
+      if grid[rownum][col] >= grid[row][col] do
+        false
+      else
+        acc
+      end
+    end)
+  end
+
   def part_one(grid) do
-    Enum.reduce(1)
+    row_boundary = Map.keys(grid) |> Enum.max()
+    col_boundary = Map.keys(grid[0]) |> Enum.max()
+
+    Enum.reduce(0..row_boundary, MapSet.new(), fn rownum, acc ->
+      Enum.reduce(0..col_boundary, acc, fn colnum, acc1 ->
+        left = is_visible(:left, grid, rownum, colnum)
+        right = is_visible(:right, grid, rownum, colnum)
+        up = is_visible(:up, grid, rownum, colnum)
+        down = is_visible(:down, grid, rownum, colnum)
+
+        IO.inspect(
+          "row: #{rownum}, col: #{colnum}, left: #{left}, right: #{right}, up: #{up}, down: #{down}"
+        )
+
+        if left or right or up or down or row_boundary == rownum or col_boundary == colnum do
+          MapSet.put(acc1, {rownum, colnum})
+        else
+          acc1
+        end
+      end)
+    end)
+    |> MapSet.size()
   end
 end
--- test/adventofcode22/dayeight_test.exs
+++ test/adventofcode22/dayeight_test.exs
@@ -1,9 +1,27 @@
 defmodule Adventofcode22.DayeightTest do
   use ExUnit.Case
 
+  test "parses into a nested map" do
+    data = Adventofcode22.Dayeight.read_file("input/8_easy.txt")
+
+    assert data == %{
+             0 => %{0 => "3", 1 => "0", 2 => "3", 3 => "7", 4 => "3"},
+             1 => %{0 => "2", 1 => "5", 2 => "5", 3 => "1", 4 => "2"},
+             2 => %{0 => "6", 1 => "5", 2 => "3", 3 => "3", 4 => "2"},
+             3 => %{0 => "3", 1 => "3", 2 => "5", 3 => "4", 4 => "9"},
+             4 => %{0 => "3", 1 => "5", 2 => "3", 3 => "9", 4 => "0"}
+           }
+  end
+
   test "part one easy" do
     data = Adventofcode22.Dayeight.read_file("input/8_easy.txt")
     res = Adventofcode22.Dayeight.part_one(data)
-    assert res == 1
+    assert res == 21
+  end
+
+  test "part one regular" do
+    data = Adventofcode22.Dayeight.read_file("input/8.txt")
+    res = Adventofcode22.Dayeight.part_one(data)
+    assert res == 1803
   end
 end
