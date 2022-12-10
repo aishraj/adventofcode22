e57705405a2a31ff6ef0d8519282868e565c4605
Aish <aish@aishraj.com>
2022-12-10 10:42:36 -0800

    day 10

--- lib/adventofcode22/dayten.ex
+++ lib/adventofcode22/dayten.ex
@@ -1,6 +1,6 @@
 defmodule Adventofcode22.Dayten do
   def part_one(data) do
-    {_, _, res} = Adventofcode22.Dayten.signal_strengths(data, [20, 60, 100, 140, 180, 220])
+    {_, _, res, _} = Adventofcode22.Dayten.signal_strengths(data, [20, 60, 100, 140, 180, 220])
     Enum.sum(res)
   end
 
@@ -26,15 +26,40 @@ defmodule Adventofcode22.Dayten do
     |> parse_file()
   end
 
+  def part_two(data, suffix \\ "real") do
+    ### The following is a dirty hack to get the answer for part 2, I would not recommend this approach
+    ## A better approach would be to use Enum.reduce or similar to build the output string but I'm lazy right now :P
+    {_, _, _, register_states} = Adventofcode22.Dayten.signal_strengths(data, [])
+    File.rm("input/10_2_" <> suffix <> ".txt")
+    {:ok, file} = File.open("input/10_2_" <> suffix <> ".txt", [:write])
+
+    for row <- 0..5 do
+      for col <- 0..39 do
+        center = register_states |> Enum.at(row * 40 + col)
+
+        if col in (center - 1)..(center + 1),
+          do: IO.binwrite(file, "#"),
+          else: IO.binwrite(file, ".")
+      end
+
+      IO.binwrite(file, "\n")
+    end
+
+    File.close(file)
+
+    File.read!("input/10_2_" <> suffix <> ".txt")
+  end
+
   def signal_strengths(instructions, watch_total_cycles) do
-    {final_x, final_cycle, strengths} =
-      Enum.reduce(instructions, {1, 1, []}, fn instruction, {x, current_cycle, strengths} ->
+    {final_x, final_cycle, strengths, register_states} =
+      Enum.reduce(instructions, {1, 1, [], [1]}, fn instruction,
+                                                    {x, current_cycle, strengths, register_states} ->
         total_cycles_for_instruction = cycles(instruction)
 
         Enum.reduce(
           1..total_cycles_for_instruction,
-          {x, current_cycle, strengths},
-          fn current_instruction_cycle_count, {x, current_cycle, strengths} ->
+          {x, current_cycle, strengths, register_states},
+          fn current_instruction_cycle_count, {x, current_cycle, strengths, register_states} ->
             strengths =
               if Enum.member?(watch_total_cycles, current_cycle) do
                 strengths ++ [x * current_cycle]
@@ -49,12 +74,14 @@ defmodule Adventofcode22.Dayten do
                 x
               end
 
-            {x, current_cycle + 1, strengths}
+            register_states = register_states ++ [x]
+
+            {x, current_cycle + 1, strengths, register_states}
           end
         )
       end)
 
-    {final_x, final_cycle, strengths}
+    {final_x, final_cycle, strengths, register_states}
   end
 
   defp apply_instruction({:noop, _}, x), do: x
--- test/adventofcode22/dayten_test.exs
+++ test/adventofcode22/dayten_test.exs
@@ -22,12 +22,12 @@ defmodule Adventofcode22.DaytenTest do
     data = "noop\naddx 3\naddx -5"
     result = Adventofcode22.Dayten.parse_file(data) |> Adventofcode22.Dayten.signal_strengths([])
 
-    assert result == {-1, 6, []}
+    assert result == {-1, 6, [], [1, 1, 1, 4, 4, -1]}
   end
 
   test "returns signal strengths for a test file" do
     data = Adventofcode22.Dayten.read_file("input/10_easy.txt")
-    {_, _, result} = Adventofcode22.Dayten.signal_strengths(data, [20, 60, 100, 140, 180, 220])
+    {_, _, result, _} = Adventofcode22.Dayten.signal_strengths(data, [20, 60, 100, 140, 180, 220])
     assert result == [420, 1140, 1800, 2940, 2880, 3960]
   end
 
@@ -39,5 +39,17 @@ defmodule Adventofcode22.DaytenTest do
   test "returns the final answer for real file" do
     data = Adventofcode22.Dayten.read_file("input/10.txt")
     assert Adventofcode22.Dayten.part_one(data) == 17180
+  end
+
+  test "part 2 easy" do
+    data = Adventofcode22.Dayten.read_file("input/10_easy.txt")
+    expected = File.read!("input/10_2_expected.txt")
+    assert Adventofcode22.Dayten.part_two(data) == expected
+  end
+
+  test "part 2" do
+    data = Adventofcode22.Dayten.read_file("input/10.txt")
+    expected = File.read!("input/10_2_real_expected.txt")
+    assert Adventofcode22.Dayten.part_two(data) == expected
   end
 end
