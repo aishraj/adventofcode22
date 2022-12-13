5fad0cd4dcc24b2e98b8be85b88cb96d979fb2ba
Aish <aish@aishraj.com>
2022-12-12 22:03:04 -0800

    13/1

--- test/adventofcode22/daythirteen_test.exs
+++ test/adventofcode22/daythirteen_test.exs
@@ -1,10 +1,15 @@
 defmodule Adventofcode22.DaythirteenTest do
   use ExUnit.Case
 
-  # test "part 1 easy" do
-  #   input = File.read!("input/13_easy.txt") |> String.split("\n\n", trim: true)
-  #   assert Adventofcode22.Daythirteen.part_one(input) == 13
-  # end
+  test "part 1 easy" do
+    input = File.read!("input/13_easy.txt") |> String.split("\n\n", trim: true)
+    assert Adventofcode22.Daythirteen.part_one(input) == 13
+  end
+
+  test "part 1 real" do
+    input = File.read!("input/13.txt") |> String.split("\n\n", trim: true)
+    assert Adventofcode22.Daythirteen.part_one(input) == 6046
+  end
 
   test "runs comparision correctly" do
     one = [[1], [2, 3, 4]]
