d7055ae1dd181acbf3fe0bc3ced238bdc579afe9
Aish <aish@aishraj.com>
2022-12-15 19:27:55 -0800

    wip

--- lib/adventofcode22/dayfifteen.ex
+++ lib/adventofcode22/dayfifteen.ex
@@ -1,11 +1,12 @@
 defmodule Adventofcode22.Dayfifteen do
   def parse_line(line) do
-    line
-    |> String.split(~r/\D+/, trim: true)
+    Regex.scan(~r/-?\d+/, line)
+    |> List.flatten()
     |> Stream.map(&String.to_integer/1)
     |> Stream.chunk_every(2)
     |> Enum.map(fn [x, y] -> {x, y} end)
     |> Enum.chunk_every(2)
+    |> List.first()
   end
 
   def parse_input(input) do
@@ -19,31 +20,44 @@ defmodule Adventofcode22.Dayfifteen do
     abs(x1 - x2) + abs(y1 - y2)
   end
 
-  @spec points_in_manhattan_distance({integer, integer}, integer) :: [{integer, integer}]
-  def points_in_manhattan_distance({x, y}, distance) do
-    for x2 <- (x - distance)..(x + distance),
-        y2 <- (y - distance)..(y + distance),
-        manhattan_distance({x, y}, {x2, y2}) == distance,
-        do: {x2, y2}
-  end
+  # @spec points_in_manhattan_distance({integer, integer}, integer) :: [{integer, integer}]
+  # def points_in_manhattan_distance({x, y}, distance) do
+  #   for x2 <- (x - distance)..(x + distance),
+  #       y2 <- (y - distance)..(y + distance),
+  #       manhattan_distance({x, y}, {x2, y2}) == distance,
+  #       do: {x2, y2}
+  # end
 
-  def part_one(input) do
+  def part_one(input, yval) do
     distance = Stream.map(input, fn [sensor, beacon] -> manhattan_distance(sensor, beacon) end)
 
-    min_x =
-      Stream.zip(input, distance)
-      |> Stream.map(fn {[{sensorx, _sensory}, {_beaconx, _beacony}], distance} ->
-        sensorx - distance
-      end)
-      |> Enum.min()
+    min_x = find_min_max(input, distance, &(&1 - &2), &Enum.min/1)
+    max_x = find_min_max(input, distance, &(&1 + &2), &Enum.max/1)
 
-    max_x =
-      Stream.zip(input, distance)
-      |> Stream.map(fn {[{sensorx, _sensory}, {_beaconx, _beacony}], distance} ->
-        sensorx + distance
-      end)
-      |> Enum.max()
+    occupied =
+      for x <- min_x..max_x,
+          Stream.zip(input, distance)
+          |> Enum.any?(fn {[sensor, _beacon], distance} ->
+            manhattan_distance(sensor, {x, yval}) <= distance
+          end),
+          do: x
 
-    {min_x, max_x}
+    beacons_at_y_10 =
+      input
+      |> Stream.map(fn [_sensor, beacon] -> beacon end)
+      |> Stream.filter(fn {_beaconx, beacony} -> beacony == yval end)
+      |> Enum.uniq()
+
+    beacons_at_y_10 = Enum.count(beacons_at_y_10)
+
+    Enum.count(occupied) - beacons_at_y_10
+  end
+
+  defp find_min_max(input, distance, joiner, reducer) do
+    Stream.zip(input, distance)
+    |> Stream.map(fn {[{sensorx, _sensory}, {_beaconx, _beacony}], distance} ->
+      joiner.(sensorx, distance)
+    end)
+    |> reducer.()
   end
 end
--- test/adventofcode22/dayfifteen_test.exs
+++ test/adventofcode22/dayfifteen_test.exs
@@ -6,30 +6,18 @@ defmodule Adventofcode22.DayfifteenTest do
     assert Adventofcode22.Dayfifteen.parse_line(line) == [{2, 18}, {-2, 15}]
   end
 
-  test "returns all points in manhattan distance" do
-    assert Adventofcode22.Dayfifteen.points_in_manhattan_distance({0, 0}, 1) == [
-             {-1, 0},
-             {0, -1},
-             {0, 1},
-             {1, 0}
-           ]
-
-    assert Adventofcode22.Dayfifteen.points_in_manhattan_distance({0, 0}, 2) == [
-             {-2, 0},
-             {-1, -1},
-             {-1, 1},
-             {0, -2},
-             {0, 2},
-             {1, -1},
-             {1, 1},
-             {2, 0}
-           ]
-  end
-
   test "for easy input" do
     raw_data = File.read!("input/15_easy.txt")
     parsed_input = Adventofcode22.Dayfifteen.parse_input(raw_data)
-    result = Adventofcode22.Dayfifteen.part_one(parsed_input)
+    result = Adventofcode22.Dayfifteen.part_one(parsed_input, 10)
     assert result == 26
+  end
+
+  @tag :skip
+  test "for real input" do
+    raw_data = File.read!("input/15.txt")
+    parsed_input = Adventofcode22.Dayfifteen.parse_input(raw_data)
+    result = Adventofcode22.Dayfifteen.part_one(parsed_input, 2_000_000)
+    assert result == 5_083_287
   end
 end
