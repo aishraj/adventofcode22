078ed5fc7bde6b45ca02ff5c0eaaa26fd3eedae0
Aish <aish@aishraj.com>
2022-12-16 17:16:48 -0800

    16 - all pairs shortest path using bfs

--- lib/adventofcode22/daysixteen.ex
+++ lib/adventofcode22/daysixteen.ex
@@ -3,6 +3,40 @@ defmodule Adventofcode22.Daysixteen do
     defstruct [:name, :children, :weight]
   end
 
+  @spec bfs(Node.t(), Map.t()) :: [{String.t(), integer()}]
+  def bfs(source, graph) do
+    queue = [{source, 0}]
+    visited = MapSet.new()
+    distances = []
+    bfs_loop(queue, visited, distances, graph)
+  end
+
+  def all_pairs_bfs(graph) do
+    graph
+    |> Map.values()
+    |> Enum.map(fn node -> %{node.name => Map.new(bfs(node, graph))} end)
+    |> Enum.reduce(%{}, fn nodemap, acc ->
+      Map.merge(acc, nodemap)
+    end)
+  end
+
+  def bfs_loop([], _visited, distances, _graph), do: distances
+
+  def bfs_loop([{current_node, current_distance} | rest_in_queue], visited, distances, graph) do
+    if MapSet.member?(visited, current_node.name) do
+      bfs_loop(rest_in_queue, visited, distances, graph)
+    else
+      visited = MapSet.put(visited, current_node.name)
+      distances = distances ++ [{current_node.name, current_distance}]
+
+      children =
+        current_node.children
+        |> Enum.map(fn child -> {Map.fetch!(graph, child), current_distance + 1} end)
+
+      bfs_loop(rest_in_queue ++ children, visited, distances, graph)
+    end
+  end
+
   @spec parse_input(String.t()) :: Map.t()
   def parse_input(raw_string) do
     raw_string
